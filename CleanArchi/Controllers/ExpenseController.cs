52fbee74cf49010fce86f440887937ff7c004877
Mickael LY <mickael.ly@syfadis.fr>
2025-12-13 14:00:23 +0100

    configure scalar and first controller

--- CleanArchi/Controllers/ExpenseController.cs
+++ CleanArchi/Controllers/ExpenseController.cs
@@ -1,83 +1,23 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CleanArchi.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchi.Web.Controllers
 {
-    public class ExpenseController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpenseController : ControllerBase
     {
-        // GET: ExpenseController
-        public ActionResult Index()
+        [HttpGet]
+        public Expense Get(Guid guid)
         {
-            return View();
-        }
-
-        // GET: ExpenseController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ExpenseController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ExpenseController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+            return new Expense
             {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ExpenseController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ExpenseController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ExpenseController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ExpenseController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+                Id = guid,
+                Description = "Sample Expense",
+                Amount = 100.00m,
+                Date = DateTime.UtcNow,
+                UserId = Guid.NewGuid()
+            };
         }
     }
 }
